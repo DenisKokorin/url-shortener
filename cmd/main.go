c0111c742c2a445db8922fc8b5cc793461062df2
denis <denis22-05@mail.ru>
2026-02-12 16:27:44 +0300

    init chi server

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"url-shortener/internal/config"
 	"url-shortener/pkg/logger"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	log := logger.SetupLogger(cfg.Env)
+
+	log.Info(
+		"starting app",
+		slog.String("env", cfg.Env),
+	)
+
+	router := chi.NewRouter()
+
+	router.Post("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("created")) })
+	router.Get("/{alias}", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("OK")) })
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,5 +1,10 @@
 package logger
 
+import (
+	"log/slog"
+	"os"
+)
+
 const (
 	envLocal = "local"
 	envDev   = "dev"
@@ -12,8 +17,8 @@ func SetupLogger(env string) *slog.Logger {
 	switch env {
 	case envLocal:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
-		) 
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
