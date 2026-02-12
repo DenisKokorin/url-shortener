b4c4f4be2821b20807f6d5fd60947382a01b3f6e
denis <denis22-05@mail.ru>
2026-02-12 16:16:50 +0300

    setup config and logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"url-shortener/internal/config"
+	"url-shortener/pkg/logger"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	log := logger.SetupLogger(cfg.Env)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env        string `yaml:"env" env-default:"local"`
+	Storage    string `yaml:"storage" env-default:"memory"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"30s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		panic("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic(fmt.Sprintf("config file does not exist: %s", configPath))
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic(fmt.Sprintf("cannot read config: %s", err))
+	}
+
+	return &cfg
+}
