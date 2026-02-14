f10e0bb94e7154d305ffab37576a054f36fe51e4
denis <denis22-05@mail.ru>
2026-02-15 00:02:08 +0300

    change sql to pgx

--- internal/storage/postgres/storage.go
+++ internal/storage/postgres/storage.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
 	"url-shortener/internal/storage"
@@ -10,6 +9,7 @@ import (
 
 	_ "github.com/lib/pq"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
@@ -18,11 +18,11 @@ const (
 )
 
 type Storage struct {
-	db *sql.DB
+	db *pgx.Conn
 }
 
 func New(path string) (*Storage, error) {
-	db, err := sql.Open("postgres", path)
+	db, err := pgx.Connect(context.Background(), path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to DB: %w", err)
 	}
@@ -36,12 +36,9 @@ func New(path string) (*Storage, error) {
 }
 
 func (s *Storage) SaveURL(ctx context.Context, url string, alias string) error {
-	stmt, err := s.db.PrepareContext(ctx, "INSERT INTO url(alias, url) VALUES($1, $2)")
-	if err != nil {
-		return fmt.Errorf("failed to prepare sql statement: %w", err)
-	}
+	stmt := "INSERT INTO url(alias, url) VALUES($1, $2)"
 
-	_, err = stmt.ExecContext(ctx, alias, url)
+	_, err := s.db.Exec(ctx, stmt, alias, url)
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) {
@@ -57,16 +54,13 @@ func (s *Storage) SaveURL(ctx context.Context, url string, alias string) error {
 }
 
 func (s *Storage) GetLongURL(ctx context.Context, alias string) (string, error) {
-	stmt, err := s.db.PrepareContext(ctx, "SELECT url FROM url WHERE alias = $1")
-	if err != nil {
-		return "", fmt.Errorf("failed to prepare sql statement: %w", err)
-	}
+	stmt := "SELECT url FROM url WHERE alias = $1"
 
 	var resURL string
 
-	err = stmt.QueryRowContext(ctx, alias).Scan(&resURL)
+	err := s.db.QueryRow(ctx, stmt, alias).Scan(&resURL)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return "", storage.ErrURLNotFound
 		}
 
